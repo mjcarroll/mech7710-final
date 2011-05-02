11d7ee3be16069f9189ce90c00921a3da2adc64e
William Woodall <wjwwood@gmail.com>
2011-05-01 23:42:37 -0500

    covariance calculations.

---
+++ imu/imu_roll.m
@@ -0,0 +1,15 @@
+% Final Project - Imu Data Analysis
+% William Woodall & Michael Carroll
+% May 6th, 2011
+% MECH 7710 Optimal Control and Estimation
+
+clear; clc; close all; format loose; format short;
+
+%% Import Static Data
+% 
+
+importfile('imu_data2.csv');
+
+%%
+%
+
